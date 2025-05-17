3da5e594a7cb789deb058c33d01d6f2a7a211016
Isa-abc <isabellabstudies@gmail.com>
2025-05-17 15:13:46 -0300

    Criando tabela de produtos

--- bd.sql
+++ bd.sql
@@ -7,3 +7,10 @@ create table usuarios(
   nome varchar(100) not null,
   email varchar(100)
 );
+
+create table produtos(
+  id_prod int primary key auto_increment,
+  nome_prod varchar(150) not null,
+  preco_prod decimal(5,2) not null,
+  desc_prod varchar(250)
+);
